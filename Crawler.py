55b72610228b3c54141cd818be4fecb2f9714624
Luca <atreb92@gmail.com>
2019-05-04 16:42:00 +0200

    Aggiunta funzione per il conteggio dei caratteri presenti nella descrizione

--- Crawler.py
+++ Crawler.py
@@ -9,11 +9,17 @@ response = requests.get(url)
 bs4 = BeautifulSoup(response.text, 'html.parser')
 
 
+
+#metodo per il conteggio dei caratteri presenti nella descrizione
+def description_len(soup):
+    text_total=0
+    for p in soup.find('div', class_="full-description js-full-description responsive-media formatted-lists").find_all('p'):
+        text_total += len(p.get_text())
+    return text_total
+
 #metodo per contare il numero totale di immagini presenti nella pagina
 def count_image(soup):
     count_img = 0
     for e in soup.find_all('div', class_='template asset'):
-        count_img = count_img + 1
+        count_img += 1
     return count_img
-
-print(count_image(bs4))
